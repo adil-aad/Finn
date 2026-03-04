6a34c47ce752aa6c9cc6fd41189b0f95849fe6db
adil-aad <131954071+adil-aad@users.noreply.github.com>
2026-03-04 17:24:07 +0300

    feat: Chatbox features

--- client/src/components/ChatBox.jsx
+++ client/src/components/ChatBox.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useAppContext } from '../context/AppContext'
+import { assets } from '../assets/assets'
 
 const ChatBox = () => {
 
@@ -14,7 +15,27 @@ const ChatBox = () => {
         }
     },[selectedChat])
   return (
-    <div>ChatBox</div>
+    <div className='flex-1 flex flex-col justify-between m-5 md:m-10 xl:mx-30
+    max-md:mt-14 2xl:pr-40'>
+        {/* chats*/}
+
+        <div className='flex-1 mb-5 overflow-y-scroll'>
+            {messages.length === 0 && (
+                <div>
+                    <img src={theme === 'dark' ? assets.logo_full: assets.logo_full_dark} alt="" 
+                    className='w-full max-w-56 sm:max-w-68'/>
+                    <p className='mt-5 text-4xl sm:text-6xl text-center text-gray-400
+                    dark:text-white'>Ask Away</p>
+                </div>
+            )}
+
+        </div>
+
+        {/* prompt box*/}
+        <form action="">
+
+        </form>
+    </div>
   )
 }
 
