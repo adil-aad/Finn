05db96f7cd8b97d94bca6bfb851d992a301e645f
adil-aad <131954071+adil-aad@users.noreply.github.com>
2026-03-07 18:16:17 +0300

    Input field for Prompt

--- client/src/components/ChatBox.jsx
+++ client/src/components/ChatBox.jsx
@@ -59,6 +59,8 @@ const ChatBox = () => {
                 <option className='dark:bg-purple-900' value="text">Text</option>
                 <option className='dark:bg-purple-900' value="image">Image</option>
             </select>
+            <input onChange={(e)=>setPrompt(e.target.value)} value={prompt} type="text" placeholder='Enter Your Prompt...'
+            className='flex-1 w-full text-sm outline-none' required/>
         </form>
     </div>
   )
