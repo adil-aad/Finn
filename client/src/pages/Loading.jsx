aa0a75345591bf4ad9ab7b80cd27acde537a805c
adil-aad <131954071+adil-aad@users.noreply.github.com>
2026-03-08 18:21:49 +0300

    feat: implement loading component with navigation timeout

--- client/src/pages/Loading.jsx
+++ client/src/pages/Loading.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Loading = () => {
+
+  const navigate = useNavigate()
+
+  useEffect(()=>{
+    const timeout = setTimeout(()=>{
+      navigate('/')
+    }, 8000)
+    return ()=> clearTimeout(timeout)
+  },[])
   return (
     <div className='bg-gradient-to-b from-[#531B81] to-[#29184B] backdrop-opacity-60
     flex-items-center justify-center h-screen w-screen text-white text-2xl'>
