d20984906a989acf5fa178e18ddb6612d1ad644b
adil-aad <131954071+adil-aad@users.noreply.github.com>
2026-03-08 18:26:19 +0300

    feat: add loading page with navigation handling and fix CSS class typo

--- client/src/pages/Loading.jsx
+++ client/src/pages/Loading.jsx
@@ -13,7 +13,7 @@ const Loading = () => {
   },[])
   return (
     <div className='bg-gradient-to-b from-[#531B81] to-[#29184B] backdrop-opacity-60
-    flex-items-center justify-center h-screen w-screen text-white text-2xl'>
+    flex items-center justify-center h-screen w-screen text-white text-2xl'>
       <div className='w-10 h-10 rounded-full border-3 border-white border-t-transparent
       animate-spin'>
 
