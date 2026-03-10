1a803f6ceff06c732eb2f7761a286f5844d04a3b
adil-aad <131954071+adil-aad@users.noreply.github.com>
2026-03-10 17:21:42 +0300

    feat: add rendering of plans in Credits component

--- client/src/pages/Credits.jsx
+++ client/src/pages/Credits.jsx
@@ -22,6 +22,16 @@ const Credits = () => {
     lg:px:8 py-12'>
       <h2 className='text-3xl font-semibold text-center mb-10 xl:mt-30
       text-gray-800 dark:text-white'>Credit Plans</h2>
+
+      <div className='flex flex-wrap justify-center gap-8'>
+        {plans.map((plan)=>(
+          <div key={plan._id} className={`border border-gray-200 dark:border-purple-700 rounded-lg
+          shadow hover:shadow-lg transition-shadow p-6 min-w-[300px] flex flex-col
+          ${plan._id === 'pro' ? "bg-purple-50 dark:bg-purple-900" : "bg-white dark:bg-transparent"}`}>
+
+          </div>
+        ))}
+      </div>
       
     </div>
   )
