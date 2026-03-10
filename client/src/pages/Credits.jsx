788dce8f0d523dac3c17376f3bfabdd51445d729
adil-aad <131954071+adil-aad@users.noreply.github.com>
2026-03-10 17:11:47 +0300

    feat: implement loading state and fetch plans in Credits component

--- client/src/pages/Credits.jsx
+++ client/src/pages/Credits.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { dummyPlans } from '../assets/assets'
+import Loading from './Loading'
 
 const Credits = () => {
+
+  const [plans, setPlans] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  const fetchPlans = async (params) => {
+    setPlans(dummyPlans)
+    setLoading(false)
+  }
+
+  useEffect(()=>{
+    fetchPlans()
+  },[])
+
+  if(loading) return <Loading />
   return (
-    <div>Credits</div>
+    <div>
+      
+    </div>
   )
 }
 
