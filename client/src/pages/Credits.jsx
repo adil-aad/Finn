14d71bfe7b33c2d945d636fe25d3557600a6c2cd
adil-aad <131954071+adil-aad@users.noreply.github.com>
2026-03-10 17:14:08 +0300

    feat: enhance layout and styling of Credits component

--- client/src/pages/Credits.jsx
+++ client/src/pages/Credits.jsx
@@ -18,7 +18,10 @@ const Credits = () => {
 
   if(loading) return <Loading />
   return (
-    <div>
+    <div className='max-w-7xl h-screen overflow-y-scroll mx-auto px-4 sm:px-6
+    lg:px:8 py-12'>
+      <h2 className='text-3xl font-semibold text-center mb-10 xl:mt-30
+      text-gray-800 dark:text-white'>Credit Plans</h2>
       
     </div>
   )
