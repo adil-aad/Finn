32be443e46fe5b9bd05f1ac2494e46dac0a03da8
adil-aad <131954071+adil-aad@users.noreply.github.com>
2026-03-14 15:46:01 +0300

    feat(server): initialize server with Express and necessary dependencies
    - Created package.json for server configuration with dependencies including express, cors, and mongoose.
    - Added server.js to set up a basic Express server with CORS and JSON middleware.
    - Implemented a root route that responds with a simple message.

---
+++ server/server.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import 'dotenv/config'
+import cors from 'cors'
+
+
+const app = express()
+
+//middleware
+app.use(cors())
+app.use(express.json())
+
+
+//route
+
+app.get('/', (req, res)=> res.send('Server is Running'))
+
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
