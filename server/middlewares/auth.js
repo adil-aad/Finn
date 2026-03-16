8c984461b5159b2bf96250e8cf7fd6dbfd5de1ab
adil-aad <131954071+adil-aad@users.noreply.github.com>
2026-03-16 18:17:17 +0300

    feat(auth): implement middleware to protect routes with JWT authentication

---
+++ server/middlewares/auth.js
@@ -0,0 +1,22 @@
+import jwt, { decode } from 'jsonwebtoken'
+import User from '../models/User.js'
+
+export const protect = async (req, res, next) => {
+    let token = req.headers.authorization
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        const userId = decoded.userId
+
+        const user = await User.findById(userId)
+
+        if(!user){
+            return res.json({success:false, message: "User not found"})
+        }
+        req.user = user
+
+        next()
+    } catch (error) {
+        res.status(401).json({message: "Not authorized, token Failed"})
+    }
+}
