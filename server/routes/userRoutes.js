c1c04bcb46358842523231abe170bac386a94913
adil-aad <131954071+adil-aad@users.noreply.github.com>
2026-03-17 16:38:40 +0300

    fix(models): correct type declaration for credits in user schema fix(routes): add missing file extensions in user routes imports

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express'
-import { getUser, loginUser, registerUser } from '../controllers/userController'
-import { protect } from '../middlewares/auth'
+import { getUser, loginUser, registerUser } from '../controllers/userController.js'
+import { protect } from '../middlewares/auth.js'
 
 const userRouter = express.Router()
 
