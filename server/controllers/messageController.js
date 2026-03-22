e721aee73b3b735573dadc7c238fd551ae20fa66
adil-aad <131954071+adil-aad@users.noreply.github.com>
2026-03-22 21:18:54 +0300

    feat: implement openai api in textMessageController

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,6 +1,7 @@
 // ai chat controller
 
 import Chat from "../models/Chat.js"
+import User from "../models/User.js"
 
 
 export const textMessageController = async (req, res) => {
@@ -9,8 +10,23 @@ export const textMessageController = async (req, res) => {
         const {chatId, prompt} = req.body
 
         const chat = await Chat.findOne({userId, _id: chatId})
-        chat.messages.push({role: "Uesr", content: prompt, timestamp: Date.now(), isImage: false})
-    } catch (error) {
+        chat.messages.push({role: "User", content: prompt, timestamp: Date.now(), isImage: false})
+
+        const {choices} = await openai.chat.completions.create({
+            model: "gemini-3-flash", // Use the Gemini model name here
+            messages: [{ role: "user", content: prompt }],
+        });
+
+        const reply = {...choices[0].message, timestamp: Date.now(), isImage:false}
+
+        res.json({success:true, reply})
+
+        chat.messages.push(reply)
+        await Chat.save()
+
+        await User.updateOne({_id: userId}, {$inc: {credites: -1}})
         
+    } catch (error) {
+        res.json({success: false, message: error.message})
     }
 }
