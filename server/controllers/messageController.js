9b06a1e1cf89c80f7b6b5f123dd75bf77e9ede90
adil-aad <131954071+adil-aad@users.noreply.github.com>
2026-03-27 21:04:42 +0300

    feat:complete messageController

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,7 +1,9 @@
 // ai chat controller
 
+import axios from "axios"
 import Chat from "../models/Chat.js"
 import User from "../models/User.js"
+import imagekit from "../configs/imageKit.js"
 
 export const getMessages = async (req, res) => {
     try {
@@ -63,9 +65,54 @@ export const imageMessageController = async (req, res) => {
         const chat = await Chat.findOne({userId, _id:chatId})
 
         chat.messages.push({
-            role: "User", content: prompt, timestamp: Date.now(), isImage: false
+            role: "User",
+            content: prompt,
+            timestamp: Date.now(),
+            isImage: false
         })
+
+        // encode the prompt 
+
+        const encodedPrompt = encodeURIComponent(prompt)
+
+        // construct ImageKit generation URL
+
+        const generatedImageUrl = `${process.env.IMAGEKIT_URL_ENDPOINT}/
+        ik-genimg-prompt-${encodedPrompt}/Finn/${Date.now()}.png?tr=w=800,h-800`
+
+        // Generating and fetching from imagekit
+
+        const aiImageResponse = await axios.get(generatedImageUrl, {responseType: "arraybuffer"})
+
+        // conver to base_64
+
+        const base64Image = `data:image/png;base64,
+        ${Buffer.from(aiImageResponse.data, "binary").toString('base64')}`
+
+        //upload to imageKit media library 
+
+        const uploadResponse = await imagekit.upload({
+            file: base64Image,
+            fileName: `${Date.now()}.png`,
+            folder: 'Finn'
+        })
+
+        const reply = {role: 'assistant',
+            content: uploadResponse.url,
+            timestamp: Date.now(), 
+            isImage:true,
+            isPublished
+        }
+
+        res.json({success:true, reply})
+
+        chat.messages.push(reply)
+        await chat.save()
+
+        await User.updateOne({_id: userId}, {$inc: {credits: -2}})
+
+
     } catch (error) {
-        
+        res.json({success:false, message: error.message})
     }
 }
