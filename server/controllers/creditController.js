96bfbbeb1bcb5bede22eaf587b88319629e95cb5
adil-aad <131954071+adil-aad@users.noreply.github.com>
2026-04-04 19:38:42 +0300

    feat: implement API controller to retrieve credit plans

--- server/controllers/creditController.js
+++ server/controllers/creditController.js
@@ -27,3 +27,11 @@ const plans = [
 
 // Api Controller for getting all plans
 
+export const getPlans = async (req, res) => {
+    try {
+        res.json({success: true, plans})
+    } catch (error) {
+        res.json({success:false, message: error.message})
+    }
+}
+
