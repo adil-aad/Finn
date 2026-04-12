fe60155146b0359814e826841374c5a728d70e5e
adil-aad <131954071+adil-aad@users.noreply.github.com>
2026-04-12 22:35:45 +0300

    feat: add credit routes to server for handling credit-related API requests

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import connectDB from './configs/db.js'
 import userRouter from './routes/userRoutes.js'
 import chatRouter from './routes/chatRoutes.js'
 import messageRouter from './routes/messageRoutes.js'
+import creditRouter from './routes/creditRoutes.js'
 
 
 const app = express()
@@ -22,6 +23,7 @@ app.get('/', (req, res)=> res.send('Server is Running'))
 app.use('/api/user', userRouter)
 app.use('/api/chat', chatRouter)
 app.use('/api/message', messageRouter)
+app.use('/api/credit', creditRouter)
 
 
 const PORT = process.env.PORT || 3000
